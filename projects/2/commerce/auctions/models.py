02116311e72369b1799199fa8f3199f66d0a6cb7
JA052464 <Jonathan.Adams@cerner.com>
2020-08-30 18:35:17 -0500

    created initial structure for tables

--- projects/2/commerce/auctions/models.py
+++ projects/2/commerce/auctions/models.py
@@ -4,3 +4,36 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    name = models.CharField(max_length=40)
+    asking_price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return f"{self.name} {self.asking_price} {self.description}"
+
+
+class Comment(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comment_item')
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.username} {self.description}"
+
+
+class Bid(models.Model):
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
+    bid_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    bid_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.item} {self.bid_amount} {self.bid_time}"
+
+
+class Watchlist(models.Model):
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='watch_item')
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watcher')
